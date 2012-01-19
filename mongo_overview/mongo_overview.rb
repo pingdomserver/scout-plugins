cb69f9e5e774946930379490d61ca2c2270e93e9
Mark S. Weiss <marksweiss@yahoo.com>
2012-01-18 23:51:17 -0500

    Backed out replica set changes

--- mongo_overview/mongo_overview.rb
+++ mongo_overview/mongo_overview.rb
@@ -73,7 +73,6 @@ class MongoOverview < Scout::Plugin
     
     # Try to connect to the database
     @db = connection.db(@database)
-    @admin_db = connection.db('admin')
     begin 
       @db.authenticate(@username,@password) unless @username.nil?
     rescue Mongo::AuthenticationError
@@ -124,50 +123,6 @@ class MongoOverview < Scout::Plugin
     counter(:op_updates, stats['opcounters']['update'], :per => :second)
     counter(:op_deletes, stats['opcounters']['delete'], :per => :second)
     counter(:op_get_mores, stats['opcounters']['getmore'], :per => :second)
-  end
-  
-  def get_replica_set_status
-    replset_status = @admin_db.command({'replSetGetStatus' => 1}, :check_response => false)
-    return unless replset_status['ok'] == 1
-    
-    member_state = case replset_status['myState']
-      when 0 
-        'Starting Up'
-      when 1 
-        'Primary'
-      when 2 
-        'Secondary'
-      when 3 
-        'Recovering'
-      when 4 
-        'Fatal'
-      when 5 
-        'Starting up (forking threads)'
-      when 6 
-        'Unknown'
-      when 7 
-        'Arbiter'
-      when 8 
-        'Down'
-      when 9 
-        'Rollback'
-    end
-    
-    report(:replset_name => replset_status['set'])
-    report(:replset_member_state => member_state)
-    report(:replset_member_state_num => replset_status['myState'])
-
-    primary = replset_status['members'].detect {|member| member['state'] == 1}
-    if primary
-      current_member = replset_status['members'].detect do |member|
-        member['self']
-      end
-      
-      if current_member
-        report(:replset_replication_lag => current_member['optimeDate'] - primary['optimeDate'])
-      end
-    end  
-    report(:replset_member_healthy => current_member['health'] ? 'True' : 'False')
   end
   
   # Handles 3 metrics - a counter for the +divended+ and +divisor+ and a ratio, named +ratio_name+, 
