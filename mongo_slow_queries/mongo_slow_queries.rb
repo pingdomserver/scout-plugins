c2df4e662e6f374f1c9edd13ad484fa57dc17ff7
Matt Wormley <mwormley@adobe.com>
2014-03-06 13:58:35 -0800

    we expect some slow queries so we just want an accurate count

--- mongo_slow_queries/mongo_slow_queries.rb
+++ mongo_slow_queries/mongo_slow_queries.rb
@@ -46,9 +46,6 @@ class ScoutMongoSlow < Scout::Plugin
       attributes: advanced
   EOS
 
-  # In order to limit the alert body size, only the first +MAX_QUERIES+ are listed in the alert body. 
-  MAX_QUERIES = 10
-
   def enable_profiling(db)
     # set to slow_only or higher (>100ms)
     if db.profiling_level == :off
@@ -88,32 +85,14 @@ class ScoutMongoSlow < Scout::Plugin
     db.authenticate(option(:username), option(:password)) if !option(:username).to_s.empty?
     enable_profiling(db)
 
-    slow_queries = []
-    last_run = memory(:last_run) || Time.now
-    current_time = Time.now
-
-    # info
-    selector = { 'millis' => { '$gte' => threshold } }
-    cursor = Mongo::Cursor.new(db[Mongo::DB::SYSTEM_PROFILE_COLLECTION], :selector => selector,:slave_ok=>true).limit(20).sort([["$natural", "descending"]])
-
-    # reads most recent first
-    # {"ts"=>Wed Dec 16 02:44:03 UTC 2009, "info"=>"query twitter_follow.system.profile ntoreturn:0 reslen:1236 nscanned:8  \nquery: { query: { millis: { $gte: 5 } }, orderby: { $natural: -1 } }  nreturned:8 bytes:1220", "millis"=>57.0}
-    cursor.each do |prof|
-      ts = prof['ts']
-      break if ts < last_run
-
-      slow_queries << prof
+    slow_queries = 0
+    if @last_run
+        selector = { 'millis' => { '$gte' => threshold }, 'ts' => { '$gt' => @last_run} }
+        slow_queries = Mongo::Cursor.new(db[Mongo::DB::SYSTEM_PROFILE_COLLECTION], :selector => selector,:slave_ok=>true).count
     end
 
-    elapsed_seconds = current_time - last_run
-    elapsed_seconds = 1 if elapsed_seconds < 1
-    # calculate per-second
-    report(:slow_queries => slow_queries.size/(elapsed_seconds/60.to_f))
+    counter(:slow_queries, slow_queries, :per => :minute)
 
-    if slow_queries.any?
-      alert(build_alert(slow_queries))
-    end
-    remember(:last_run,Time.now)
   rescue Mongo::ConnectionFailure => error
     error("Unable to connect to MongoDB","#{error.message}\n\n#{error.backtrace}")
     return
@@ -123,26 +102,6 @@ class ScoutMongoSlow < Scout::Plugin
       return
     else
       raise error
-    end
-  end
-
-  def build_alert(slow_queries)
-    subj = "Maximum Query Time exceeded on #{slow_queries.size} #{slow_queries.size > 1 ? 'queries' : 'query'}"
-    # send a sampling of slow queries. the total # of queries + query is limited as scout will throwout large checkins.
-    queries = []
-    slow_queries[0..(MAX_QUERIES-1)].each do |sq|
-      if q=sq['query'] and q.size > 300
-        sq['query'] = q[0..300] + '...'
-      end
-      queries << sq
-    end
-    {:subject => subj, :body => queries.to_json}
-  end
-
-  # Override Binary's to_json command - we were getting errors trying to serialize binaries as part of the slow query report
-  class BSON::Binary
-    def to_json(*args)
-      inspect
     end
   end
 
