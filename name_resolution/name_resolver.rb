92c5e6299552e577207a05f232a9fd86ffc910fc
Patrick Robinson <patrick.robinson@envato.com>
2015-08-05 11:20:56 +1000

    Use Ruby Timeout instead of Resolv::DNS timeout

--- name_resolution/name_resolver.rb
+++ name_resolution/name_resolver.rb
@@ -8,21 +8,15 @@ class NameResolver < Scout::Plugin
     timeout:
       default: 30
   EOS
-  # Doesn't work :(
-  TIMEOUT=77
 
   def build_report
-    resolver = Resolv::DNS.new(:nameserver => [option(:nameserver)])
-
-    # Only Ruby >= 2.0.0 supports setting a timeout.
-    # Without this DNS timeouts will raise a PluginTimeoutError
-    if resolver.respond_to? :timeouts=
-      resolver.timeouts = option(:timeout)
-    end
-
     begin
-      resolver.getaddress(option(:resolve_address))
-    rescue Resolv::ResolvError, Resolv::ResolvTimeout => err
+      # Only Ruby >= 2.0.0 supports setting a timeout, so use this
+      Timeout.timeout(option(:timeout)) do
+        resolver = Resolv::DNS.new(:nameserver => [option(:nameserver)])
+        resolver.getaddress(option(:resolve_address))
+      end
+    rescue Resolv::ResolvError, Resolv::ResolvTimeout, Timeout::Error => err
       report(:resolved? => 0, :result => err)
     else
       report(:resolved? => 1)
