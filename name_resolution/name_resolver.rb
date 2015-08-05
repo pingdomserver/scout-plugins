5b23b4100136a94aafa428499846fc04e0d7e1cb
Patrick Robinson <patrick.robinson@envato.com>
2015-08-05 10:44:46 +1000

    Set timeout for newer ruby versions

--- name_resolution/name_resolver.rb
+++ name_resolution/name_resolver.rb
@@ -5,10 +5,20 @@ class NameResolver < Scout::Plugin
       default: 8.8.8.8
     resolve_address:
       default: google.com
+    timeout:
+      default: 30
   EOS
+  # Doesn't work :(
+  TIMEOUT=77
 
   def build_report
     resolver = Resolv::DNS.new(:nameserver => [option(:nameserver)])
+
+    # Only Ruby >= 2.0.0 supports setting a timeout.
+    # Without this DNS timeouts will raise a PluginTimeoutError
+    if resolver.respond_to? :timeouts=
+      resolver.timeouts = option(:timeout)
+    end
 
     begin
       resolver.getaddress(option(:resolve_address))
