e03db8f76419bd278191d1d2080b79fae8dfe139
Matt Chesler <mchesler@movableink.com>
2016-04-06 15:36:07 -0400

    Report total, active, deferred and hold queue lengths

--- mail_monitor/test.rb
+++ mail_monitor/test.rb
@@ -1,46 +1,76 @@
-require 'test/unit'
-require 'mocha/test_unit'
-require File.expand_path('../mail_monitor.rb', __FILE__)
+require File.dirname(__FILE__) + "/../test_helper.rb"
+require File.dirname(__FILE__) + "/mail_monitor.rb"
 
-class Mail_monitorTest < Test::Unit::TestCase
+class MailMonitorTest < Test::Unit::TestCase
 
   def setup
-    @plugin = Mail_monitor.new(nil, {}, @options)
+    @plugin = MailMonitor.new(nil, {}, {:mail_binary => "postqueue,sendmail,mailq"})
   end
 
   def test_successful_run_with_no_errors
-    @plugin.stubs(:get_command_result).returns(File.read(File.dirname(__FILE__)+'/fixtures/empty.txt'))
-    @plugin.stubs(:execute_command).returns(0)
+    @plugin.stubs(:execute_command).returns({
+      :exit_code => 0,
+      :output => File.read(File.dirname(__FILE__)+'/fixtures/empty.txt')
+    })
+
     res = @plugin.run()
     assert(res[:errors].empty?, "res[:errors] is not empty")
   end
 
   def test_unable_to_find_mail_bin
-    @plugin.stubs(:mail_bin_options).returns("blah")
+    @plugin.stubs(:mail_binary).returns(nil)
+
     res = @plugin.run()
-    assert(!res[:errors].empty?, "res[:errors] is not empty")
+    assert_equal res[:errors].first[:subject], "mail binary cannot be found for postqueue, sendmail, mailq"
   end
 
   def test_failed_run_with_errors
-    @plugin.stubs(:execute_command).returns(1)
+    @plugin.stubs(:execute_command).returns({
+      :exit_code => 69,
+      :output => File.read(File.dirname(__FILE__)+'/fixtures/fatal.txt')
+    })
+
     res = @plugin.run()
-    assert(!res[:errors].empty?, "res[:errors] is empty")
+    assert_equal res[:errors].first[:subject], "Bad exit code from '/usr/sbin/postqueue -p'"
+    assert_equal res[:errors].first[:body], "postqueue: fatal: Queue report unavailable - mail system is down"
   end
 
   def test_0_messages
-    @plugin.stubs(:get_command_result).returns(File.read(File.dirname(__FILE__)+'/fixtures/empty.txt'))
-    @plugin.stubs(:execute_command).returns(0)
+    @plugin.stubs(:execute_command).returns({
+      :exit_code => 0,
+      :output => File.read(File.dirname(__FILE__)+'/fixtures/empty.txt')
+    })
 
-    @plugin.run()
-    assert_equal 0, @plugin.count
+    res = @plugin.run()
+    assert_equal 0, res[:reports].first[:total]
+    assert_equal 0, res[:reports].first[:active]
+    assert_equal 0, res[:reports].first[:hold]
+    assert_equal 0, res[:reports].first[:deferred]
   end
 
   def test_more_than_0_messages
-    @plugin.stubs(:get_command_result).returns(File.read(File.dirname(__FILE__)+'/fixtures/more_than_one_message.txt'))
-    @plugin.stubs(:execute_command).returns(0)
+    @plugin.stubs(:execute_command).returns({
+      :exit_code => 0,
+      :output => File.read(File.dirname(__FILE__)+'/fixtures/more_than_one_message.txt')
+    })
 
-    @plugin.run()
-    assert_operator 0, :<,  @plugin.count
-    #assert_equal 0, @plugin.count
+    res = @plugin.run()
+    assert_equal 2, res[:reports].first[:total]
+    assert_equal 0, res[:reports].first[:active]
+    assert_equal 0, res[:reports].first[:hold]
+    assert_equal 2, res[:reports].first[:deferred]
+  end
+
+  def test_more_than_0_messages_with_queue_indicators
+    @plugin.stubs(:execute_command).returns({
+      :exit_code => 0,
+      :output => File.read(File.dirname(__FILE__)+'/fixtures/more_than_one_with_queue_indicator.txt')
+    })
+
+    res = @plugin.run()
+    assert_equal 6, res[:reports].first[:total]
+    assert_equal 3, res[:reports].first[:active]
+    assert_equal 1, res[:reports].first[:hold]
+    assert_equal 2, res[:reports].first[:deferred]
   end
 end
