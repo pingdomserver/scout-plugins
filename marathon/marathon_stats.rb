65450047a277a45322dbd4bb8360ed2c5a4137f5
Lukas Lachowski <lukasz.lachowski@solarwinds.com>
2017-07-13 19:55:51 +0200

    More conscious implementation of the plugin for Plex/Marathon/Mesos.

--- marathon/marathon_stats.rb
+++ marathon/marathon_stats.rb
@@ -5,52 +5,80 @@
 #
 
 class MarathonStats < Scout::Plugin
-  needs "net/http", "uri", "json"
+  needs "net/http", "uri", "json", "socket"
 
   OPTIONS=<<-EOS
-      server_url:
-        name: Server Status URL
-        notes: Specify URL of the server-status page to check. Scout requires the machine-readable format of the status page (just add '?auto' to the server-status page URL).
-        default: "http://localhost/server-status"
+      mesos_containers_url:
+        name: Mesos Server containers REST API URL
+        notes: Specify the URL of the server's containers REST API to check.
+        default: "http://localhost/containers"
+      statsd_address:
+        name: StatsD daemon ip address
+        notes: Specify the address for the StatsD daemon.
+        default: "127.0.0.1"
+      statsd_port:
+        name: StatsD daemon port.
+        notes: Specify the port for the StatsD daemon.
+        default: 8125
+      marathon_url:
+        name: Marathon REST API URL
+        notes: Specify the URL for the Marathon's REST API.
+        default: "http://localhost/v2/apps/"
   EOS
 
   def build_report
-    mesos_url = option("server_url")
-
-    containers = JSON.parse(make_request(mesos_url))
+    containers = get_containers()
     for container_data in containers do
-      app_id = container_data["executor_id"]
+      if !container_data.key?(:executor_id)
+        raise "Missing the 'executor_id' attribute in data payload returned by Mesos."
+      end
+      app_id = container_data[:executor_id]
       app_name = get_app_name(app_id)
       publish_statsd(container_data, app_name)
     end
   end
 
-  def get_app_name(app_id)
-    marathon_app_url = "marathon url/" + app_id
-
-    app_data = JSON.parse(make_request(marathon_app_url))
-    return app_data["id"]
+  def get_containers()
+    begin
+      mesos_uri = URI.parse(option(:mesos_url))
+      return JSON.parse(make_request(mesos_uri))
+    rescue => ex
+      error = RuntimeError.new("Error while getting the list of Mesos containers.")
+      error.set_backtrace(ex.backtrace)
+      raise error
+    end
   end
 
-  def make_request(url)
-    uri = URI.parse(url)
+  def get_app_name(app_id)
+    begin
+      marathon_app_uri = URI.parse(option(:marathon_url)).join(app_id)
+      app_data = JSON.parse(make_request(marathon_app_uri))
+      return app_data[:app][:id]
+    rescue => ex
+      error = RuntimeError.new("Error while getting application's name.")
+      error.set_backtrace(ex.backtrace)
+      raise error
+    end
+  end
+
+  def make_request(uri)
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true if uri.scheme == "https"
     req = Net::HTTP::Get.new(uri.path)
-    # req.basic_auth(option(:username), option(:password))
     response = http.request(req)
     return response.body
   end
 
   def publish_statsd(container_data, app_name)
-    statistics = container_data["statistics"]
+    if !container_data.key?(:statistics)
+      raise "Missing the 'statistics' attribute in data payload returned by Marathon."
+    end
+    statistics = container_data[:statistics]
     statsd_values = json_to_statsd(statistics, app_name)
     statsd_values.each do |statsd|
       send_statsd(statsd)
     end
   end
-
-  # STATSD = Statsd.new 'localhost', 8125
 
   def json_to_statsd(json_data, prefix)
     results = []
@@ -61,10 +89,19 @@ class MarathonStats < Scout::Plugin
   end
 
   def to_statsd_gauge(prefix, key, value)
-    return prefix + "." + key + ":" + value + "|g"
+    return "%s.%s:%s|g" % [prefix, key.to_s, value.to_s]
   end
 
   def send_statsd(statsd)
+    begin
+      scoutd_address = option(:statsd_address)
+      scoutd_port = option(:statsd_port)
+      @socket.send(statsd, 0, scoutd_address, scoutd_port)
+    rescue => ex
+      error = RuntimeError.new("Error while sending StatsD data to Scout.")
+      error.set_backtrace(ex.backtrace)
+      raise error
+    end
   end
 
 end
